35904159ec9a5e492cf436cb5ec82589dad26d36
Wes8830 <Wes8830@users.noreply.github.com>
2020-09-08 15:46:27 +0000

    automated commit by submit50

--- hello.c
+++ hello.c
@@ -3,7 +3,9 @@
 
 int main(void)
 {
+    //Prompt user for their name, pass it into 'name' string variable
     string name = get_string("What is your name?\n");
-    
+
+    //Output of name using a string placeholder
     printf("hello, %s\n", name);
 }
