b21b081ed7c7a1acff08bea63f933c0b9271faab
Wes8830 <Wes8830@users.noreply.github.com>
2020-09-09 16:38:35 +0000

    automated commit by check50 [check50=True]

--- credit.c
+++ credit.c
@@ -41,15 +41,16 @@ void checksum_validator(long cc_nbr)
         digit = cc_nbr % 10;
         sum_first = digit + sum_first;
         cc_nbr = cc_nbr / 10;
-        //printf("your current First digit is: %i\n", digit);
-        //printf("your current First digit summation is: %i\n", sum_first);
+        printf("your current First digit is: %i\n", digit);
+        printf("your current First digit summation is: %i\n", sum_first);
         digit = cc_nbr % 10;
         sum_second = (digit * 2) + sum_second;
-        //printf("your current Second digit is: %i\n", digit);
-        //printf("your current Second digit summation is: %i\n", sum_second);
+        printf("your current Second digit is: %i\n", digit);
+        printf("your current Second digit summation is: %i\n", sum_second);
         //printf("your new number is: %li\n", cc_nbr);
         count++;
-        //printf("count is: %i\n", count);
+        printf("count is: %i\n", count);
+        cc_nbr = cc_nbr / 10;
 
     }
     while (cc_nbr > 0);
@@ -58,7 +59,7 @@ void checksum_validator(long cc_nbr)
     //first validation check of whether or not it's a legit number
     if (checksum == 0)
     {
-        printf("Your checksum looks good! It is: %i\n", checksum);
+        //printf("Your checksum looks good! It is: %i\n", checksum);
         cardlength_validator(count, cc_nbr);
     } else
     {
