9f670e3b070f00760a1f21b82cb2e6f279403fa6
Wes8830 <Wes8830@users.noreply.github.com>
2020-10-04 18:13:35 +0000

    automated commit by check50 [check50=True]

--- substitution.c
+++ substitution.c
@@ -12,117 +12,133 @@
 // encipher the text
 // print cipher text
 
-int error_check(string key, int key_length, int argc);
+//int error_check(string key, int key_length, int arg_count);
 
 int main(int argc, string argv[])
 {
-    
+
     //specify [1] here becuase argv is a vector of arguments. It doesn't store one argument as a string, it stores multiple arguments in a list called argv!
     string key = argv[1];
-    printf("Your Key: %s\n", key);
-    int key_length = strlen(key);
+    int key_length = strlen(argv[1]);
+    int arg_count = argc;
+    printf("your argument count is: %i ", argc);
 
     //check for errors in their Key
-    error_check(key, key_length, argc);
-    
-    //Prompt the user for Plaintext
-    string plaintext = get_string("plaintext: ");
-
-    int plntxt_len = strlen(plaintext);
-    int plaintext_ucase = 0; //enum for 3 cases (uppercase, lowercase, non-alphabetic)
-
-    string upper_alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    string lower_alpha = "abcdefghijklmnopqrstuvwxyz";
-    int cyphertext[plntxt_len];
-
-    //Check for Errors in the Command Line Argument Input
-
-    //Index through Plaintext
-    for (int i = 0; i < plntxt_len; i++)
+    //int no_error = error_check(key, key_length, argc);
+        //check to make sure they user provided a 26 character Key
+    int no_error = 0;
+    if (arg_count < 2 || arg_count > 2)
     {
-        int n = 0;
-
-        //check upper case alphabet
-      if (plaintext[i] >= 65 && plaintext [i] <= 90)
-      {
-       plaintext_ucase = 2;
-        while (plaintext[i] != upper_alpha[n])
-        {
-            n++;
-        }
-
-      }//check lower case alphabet
-      else if (plaintext[i] >= 97 && plaintext [i] <=122)
-      {
-        plaintext_ucase = 1;
-        while (plaintext[i] != lower_alpha[n])
-        {
-            n++;
-        }
-      }
-      else
-      {
-        plaintext_ucase = 0;
-      }
-
-        //check if PLAINTEXT index matches n index in the KEY
-
-        if (plaintext_ucase == 2)
-        {
-           //ok, PLAINTEXT is uppercase, but the key could be in lowercase, if it is subtract 32 from it's ASCII string value
-           if (key[n] >= 97 && key[n] <= 122)
-            {
-                key[n] = key[n] - 32;
-            }
-            cyphertext[i] = key[n];
-        } else if ( plaintext_ucase == 1)
-        {
-            //Plaintext is lowercase, but the key could be in uppercase, if it is, add 32 to it's ASCII string index value
-            if (key[n] >= 65 && key[n] <= 90)
-            {
-                key[n] = key[n] + 32;
-            }
-            cyphertext[i] = key[n];
-
-        }
-        else
-        {
-            cyphertext[i] = plaintext[i];
-        }
-
-
+        printf("Usage: ./substitution key\n");
+        no_error = 1;
+    }
+    else if (key_length != 26)
+    {
+        printf("Key must contain 26 characters.\n");
+        no_error = 1;
+    }
+    else //no errors found
+    {
+        no_error = 0;
     }
 
-    printf("ciphertext: ");
-    for (int j = 0; j < plntxt_len; j++)
-    {
-        printf("%c",cyphertext[j]);
+    if (no_error == 0) {
+        //Prompt the user for Plaintext
+        string plaintext = get_string("plaintext: ");
 
+        int plntxt_len = strlen(plaintext);
+        int plaintext_ucase = 0; //enum for 3 cases (uppercase, lowercase, non-alphabetic)
+
+        string upper_alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        string lower_alpha = "abcdefghijklmnopqrstuvwxyz";
+        int cyphertext[plntxt_len];
+
+        //Check for Errors in the Command Line Argument Input
+
+        //Index through Plaintext
+        for (int i = 0; i < plntxt_len; i++)
+        {
+            int n = 0;
+
+            //check upper case alphabet
+          if (plaintext[i] >= 65 && plaintext [i] <= 90)
+          {
+           plaintext_ucase = 2;
+            while (plaintext[i] != upper_alpha[n])
+            {
+                n++;
+            }
+
+          }//check lower case alphabet
+          else if (plaintext[i] >= 97 && plaintext [i] <=122)
+          {
+            plaintext_ucase = 1;
+            while (plaintext[i] != lower_alpha[n])
+            {
+                n++;
+            }
+          }
+          else
+          {
+            plaintext_ucase = 0;
+          }
+
+            //check if PLAINTEXT index matches n index in the KEY
+
+            if (plaintext_ucase == 2)
+            {
+               //ok, PLAINTEXT is uppercase, but the key could be in lowercase, if it is subtract 32 from it's ASCII string value
+               if (key[n] >= 97 && key[n] <= 122)
+                {
+                    key[n] = key[n] - 32;
+                }
+                cyphertext[i] = key[n];
+            } else if ( plaintext_ucase == 1)
+            {
+                //Plaintext is lowercase, but the key could be in uppercase, if it is, add 32 to it's ASCII string index value
+                if (key[n] >= 65 && key[n] <= 90)
+                {
+                    key[n] = key[n] + 32;
+                }
+                cyphertext[i] = key[n];
+
+            }
+            else
+            {
+                cyphertext[i] = plaintext[i];
+            }
+
+
+        }
+
+        printf("ciphertext: ");
+        for (int j = 0; j < plntxt_len; j++)
+        {
+            printf("%c",cyphertext[j]);
+
+        }
+        printf("\n");
+        return 0;
     }
-    printf("\n");
-
 }
 
 
 
-int error_check(string key, int key_length, int argc)
+int error_check(string key, int key_length, int arg_count)
 {
-    string usage_error = "";
     //check to make sure they user provided a 26 character Key
-    if (key_length != 26)
+    if (arg_count < 2 || arg_count > 2)
     {
-        printf("Key must contain 26 characters.");
-        return 0;
+        printf("Usage: ./substitution key\n");
+        return 1;
     }
-    else if (argc > 2)
+    else if (key_length != 26)
     {
-        printf("Usage: ./substitution key");
-        return 0;
+        printf("Key must contain 26 characters.\n");
+        return 1;
     }
-    else
+    else //no errors found
     {
-        printf("Usage: ./substitution key");
         return 0;
-
     }
 }
