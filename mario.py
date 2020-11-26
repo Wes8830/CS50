b2d8a4dff1457817ff42e52e65af08fe88423ad7
Wes8830 <Wes8830@users.noreply.github.com>
2020-11-26 06:03:52 +0000

    automated commit by submit50

--- mario.py
+++ mario.py
@@ -1,12 +1,13 @@
 import cs50
 
-
 x = 0
+
 while x < 1 or x > 8:
     x = cs50.get_int("Height:")
 
 #print j height (rows)
 j = 1
+
 while j <= x:
     #print x - j spaces
     for i in range(0, x - j):
@@ -22,9 +23,8 @@ while j <= x:
     for m in range(0, j):
         print("#", end = "")
 
-
     #print for new row
     print("")
 
     #increment j
-    j+=1
+    j += 1
