34c9b4b7374a6727ba6f76d32c761b817751f824
Wes8830 <Wes8830@users.noreply.github.com>
2020-11-26 06:07:16 +0000

    automated commit by submit50

--- mario.py
+++ mario.py
@@ -3,28 +3,28 @@ import cs50
 x = 0
 
 while x < 1 or x > 8:
-    x = cs50.get_int("Height:")
+    x = cs50.get_int("Height: ")
 
-#print j height (rows)
+# print j height (rows)
 j = 1
 
 while j <= x:
-    #print x - j spaces
+    # print x - j spaces
     for i in range(0, x - j):
-        print(" ", end = "")
-    #print j bricks
-    for g in range(0,j):
-        print("#", end = "")
+        print(" ", end="")
+    # print j bricks
+    for g in range(0, j):
+        print("#", end="")
 
-    #print spacing between bricks
-    print("  ", end = "")
+    # print spacing between bricks
+    print("  ", end="")
 
-    #print j bricks
+    # print j bricks
     for m in range(0, j):
-        print("#", end = "")
+        print("#", end="")
 
-    #print for new row
+    # print for new row
     print("")
 
-    #increment j
+    # increment j
     j += 1
